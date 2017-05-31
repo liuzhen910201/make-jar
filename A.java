9cec1da4a289c2dbae5e83e739a71b3595fe2b7c
zhen liu <zhen liu>
2017-05-31 23:12:10 +0900

    wee

---
+++ A.java
@@ -0,0 +1,14 @@
+class B{
+    static int a=11;
+    private int b = 12;
+    //System.out.println(b);
+}
+public class A{
+    public static void main(String[] args){
+	B b1=new B();
+	System.out.println(b1.a);
+	
+    }
+}
+
+	        
